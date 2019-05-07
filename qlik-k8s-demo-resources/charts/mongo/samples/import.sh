fc371818abbbbea7dd00a6c4d275cf77af53bf0f
Steven Pressland <steven.pressland@qlik.com>
2019-05-07 22:20:24 +0100

    update load script

--- qlik-k8s-demo-resources/charts/mongo/samples/import.sh
+++ qlik-k8s-demo-resources/charts/mongo/samples/import.sh
@@ -1,7 +1,7 @@
 sleep 10
 apt-get update
 apt-get install wget unzip
-wget https://github.com/AnalyticsEarth/qlik-k8s-demo-resources/raw/master/samples/mongo.zip /tmp/
-unzip /tmp/mongo.zip
+wget -P /tmp/ https://github.com/AnalyticsEarth/qlik-k8s-demo-resources/raw/master/samples/mongo.zip
+unzip /tmp/mongo.zip -d /tmp
 mongorestore -d northwinds /tmp/mongo/northwinds/
 mongorestore -d twitter /tmp/mongo/twitter/
