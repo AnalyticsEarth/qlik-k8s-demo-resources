0e11547e4844106bf94fb51e06b5dd5d29e24c8e
Steven Pressland <steven.pressland@qlik.com>
2019-05-08 22:46:12 +0100

    add two databases and readonly user

--- samples/mssql/install.sql
+++ samples/mssql/install.sql
@@ -3,3 +3,26 @@ FROM DISK = '/demodata/AdventureWorksDW2017.bak'
 WITH MOVE 'AdventureWorksDW2017' TO '/demodata/AdventureWorksDW2017.mdf',
 MOVE 'AdventureWorksDW2017_Log' TO '/demodata/AdventureWorksDW2017_Log.ldf'
 GO
+
+RESTORE DATABASE AdventureWorks2017
+FROM DISK = '/demodata/AdventureWorks2017.bak'
+WITH MOVE 'AdventureWorks2017' TO '/demodata/AdventureWorks2017.mdf',
+MOVE 'AdventureWorks2017_Log' TO '/demodata/AdventureWorks2017_Log.ldf'
+GO
+
+CREATE LOGIN qliksense WITH PASSWORD = 'Qlik1234';
+GO
+
+-- Creates a database user for the login created above.
+CREATE USER qliksense FOR LOGIN qliksense;
+GO
+
+USE AdventureWorksDW2017
+GO
+exec sp_addrolemember 'db_datareader',qliksense
+GO
+
+USE AdventureWorksDW2017
+GO
+exec sp_addrolemember 'db_datareader',qliksense
+GO
