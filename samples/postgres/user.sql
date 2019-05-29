85a20aff529f0a9213330b6ff52da1aa1a9f6593
Steven Pressland <steven.pressland@qlik.com>
2019-05-29 12:57:20 +0100

    update files

--- samples/postgres/user.sql
+++ samples/postgres/user.sql
@@ -1,6 +1,4 @@
 
-REVOKE ALL PRIVILEGES ON SCHEMA pg_temp_1, pg_toast, pg_toast_temp_1, public FROM PUBLIC;
-
 GRANT CONNECT ON DATABASE postgresdb TO qliksense;
 GRANT USAGE ON SCHEMA world_cities TO qliksense;
 GRANT SELECT ON ALL TABLES IN SCHEMA world_cities TO qliksense;
