2f586389be5f68e02bec878be63f6f0422b4cbe0
Steven Pressland <steven.pressland@qlik.com>
2019-05-29 12:33:39 +0100

    update files

---
+++ samples/postgres/init.sh
@@ -0,0 +1,5 @@
+wget -P /demodata https://raw.githubusercontent.com/AnalyticsEarth/qlik-k8s-demo-resources/master/samples/postgres/install.sh
+wget -P /demodata https://raw.githubusercontent.com/AnalyticsEarth/qlik-k8s-demo-resources/master/samples/postgres/createdb.sql
+wget -P /demodata https://raw.githubusercontent.com/AnalyticsEarth/qlik-k8s-demo-resources/master/samples/postgres/world.sql
+wget -P /demodata https://raw.githubusercontent.com/AnalyticsEarth/qlik-k8s-demo-resources/master/samples/postgres/user.sql
+chmod +x /demodata/install.sh
